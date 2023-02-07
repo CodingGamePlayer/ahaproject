6635a6c685d58a51fdf804397ad0c1492e36f60a
048 <048@A-20>
2023-02-07 13:18:49 +0900

    commit

---
+++ src/main/java/kr/co/ahaproject/mapper/jyh/Replymapper.java
@@ -0,0 +1,61 @@
+package kr.co.ahaproject.mapper.jyh;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import kr.co.ahaproject.dto.BoardDTO;
+import kr.co.ahaproject.dto.ReplyDTO;
+import kr.co.ahaproject.entity.Board;
+import kr.co.ahaproject.entity.Reply;
+
+@Mapper
+public interface Replymapper {
+	
+	
+	
+	
+	//게시판 글에대한 댓글조회
+	@Select("select*from reply where b_id =#{b_id} order by rp_id desc")
+    @Results(id = "ReplyMap", value = {
+            @Result(property = "rp_id", column = "rp_id"),
+            @Result(property = "b_id", column = "b_id"),
+            @Result(property = "rp_body", column = "rp_body"),
+            @Result(property = "us_id", column = "us_id")})
+           
+      List<BoardDTO> listAll();
+            @Select("SELECT * FROM reply WHERE rp_id = #{reply.rp_id}")
+            @ResultMap("ReplyMap")
+            Reply findById(@Param("reply") Reply reply);
+	
+	List<ReplyDTO> getdetail(int b_id);
+	
+	
+	//댓글 선택조회
+	@Select("select* from reply where rp_id = #{rp_id}")
+	ReplyDTO replySelectOne(int rp_id);
+	
+	//댓글 작성
+	@Insert("insert into reply(rp_body, us_id) values(#{rp_body}, #{us_id}")
+	int replycreate(ReplyDTO dto);
+	
+	
+	
+	//댓글 수정
+	@Update("update reply set rp_body=#{rp_body}, us_id=#{us_id} where rp_id = #{rp_id} and b_id = #{b_id}")
+	int replyupdate(ReplyDTO dto); 
+	
+	
+	
+	//댓글 삭제
+	@Delete("delete from reply where rp_id=#{rp_id}")
+	int replydelete(int rp_id);
+}
