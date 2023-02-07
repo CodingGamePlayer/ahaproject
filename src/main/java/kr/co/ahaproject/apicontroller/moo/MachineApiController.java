74f340415f16d9c59a81702ad45db1c3f5ef0c49
CodingGamePlayer <115041502+CodingGamePlayer@users.noreply.github.com>
2023-02-07 14:55:01 +0900

    Moo (#11)
    * moo Package 생성
    
    * moo Package 생성
    
    * commit
    
    ---------
    
    Co-authored-by: moosoony <moogyong@naver.com>

---
+++ src/main/java/kr/co/ahaproject/apicontroller/moo/MachineApiController.java
@@ -0,0 +1,10 @@
+package kr.co.ahaproject.apicontroller.moo;
+
+import kr.co.ahaproject.dto.MachineDTO;
+import org.springframework.http.ResponseEntity;
+
+public class MachineApiController {
+    ResponseEntity register(MachineDTO machineDTO);
+
+
+}
---
+++ src/main/java/kr/co/ahaproject/controller/moo/MachineController.java
@@ -0,0 +1,7 @@
+package kr.co.ahaproject.controller.moo;
+
+import org.springframework.ui.Model;
+
+public interface MachineController {
+    String machineForm(Model model);
+}
