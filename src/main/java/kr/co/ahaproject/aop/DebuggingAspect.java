85fd50a53b9e986301310670a124fb5a9d2205e3
김종식 <otw1917@naver.com>
2023-02-07 12:59:10 +0900

    aop 기능 추가

--- src/main/java/kr/co/ahaproject/aop/DebuggingAspect.java
+++ src/main/java/kr/co/ahaproject/aop/DebuggingAspect.java
@@ -13,8 +13,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class DebuggingAspect {
 
-    @Pointcut("execution(* kr.co.ahaproject.apicontroller..*.*(..))")
+    @Pointcut("execution(* kr.co.ahaproject.controller..*.*(..))")
     private void cut() {
+    }
+
+    @Pointcut("execution(* kr.co.ahaproject.service..*.*(..)")
+    private void cut1(){
     }
 
     @Before("cut()")
@@ -34,6 +38,33 @@ public class DebuggingAspect {
 
     @AfterReturning(value = "cut()", returning = "returnObj")
     public void loggingReturnValue(JoinPoint joinPoint, Object returnObj) {
+
+        String simpleName = joinPoint.getTarget()
+                .getClass()
+                .getSimpleName();
+
+        String methodName = joinPoint.getSignature().getName();
+
+        log.info("{}#{}의 입력값 => {}", simpleName, methodName, returnObj);
+    }
+
+    @Before("cut1()")
+    public void loggingArgsForServ(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+
+        String simpleName = joinPoint.getTarget()
+                .getClass()
+                .getSimpleName();
+
+        String methodName = joinPoint.getSignature().getName();
+
+        for (Object obj : args) {
+            log.info("{}#{}의 입력값 => {}", simpleName, methodName, obj);
+        }
+    }
+
+    @AfterReturning(value = "cut1()", returning = "returnObj")
+    public void loggingReturnValueForServ(JoinPoint joinPoint, Object returnObj) {
 
         String simpleName = joinPoint.getTarget()
                 .getClass()
