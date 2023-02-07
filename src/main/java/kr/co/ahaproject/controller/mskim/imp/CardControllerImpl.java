d3d491de2dd0c4b2aeb29cd08c8d32beca71f8e9
oster <kms0843@naver.com>
2023-02-07 14:22:16 +0900

    cardmapper update

---
+++ src/main/java/kr/co/ahaproject/controller/mskim/imp/CardControllerImpl.java
@@ -0,0 +1,37 @@
+package kr.co.ahaproject.controller.mskim.imp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import kr.co.ahaproject.controller.mskim.CardController;
+import kr.co.ahaproject.service.mskim.CardService;
+
+@Controller
+public class CardControllerImpl implements CardController {
+	
+	@Autowired
+	private CardService cardService;
+
+	@Override
+	@GetMapping("/user/basicinfo/card")
+	public ModelAndView card(ModelAndView mav) {
+		
+		mav.addObject("cardDTOs", cardService.selectAll());
+		mav.setViewName("/user/basicinfo/card");
+		
+		return mav;
+	}
+
+	@Override
+	@GetMapping("/user/basicinfo/card")
+	public ModelAndView card(ModelAndView mav, @RequestParam("cl_code") String cl_code) {
+		
+		mav.addObject("cardDTOs", cardService.findBYClient(cl_code));
+		mav.setViewName("/user/basicinfo/card");
+		return mav;
+	}
+
+}
