c5f70958122d92e04eb1b42b808ea60ffb663cb3
CodingGamePlayer <115041502+CodingGamePlayer@users.noreply.github.com>
2023-02-07 18:08:22 +0900

    Yuri (#26)
    * - EmployeeControllerImp.java 파일 없어져서 다시 올려봄 ㅠㅠ
    
    * - sidebar 메뉴 주소 수정
    
    * - sidebar 메뉴 주소 수정
    
    * - sidebar 메뉴 주소 수정
    
    * commit
    
    ---------
    
    Co-authored-by: choi yuri <yurifrom1123@gmail.com>

---
+++ src/main/java/kr/co/ahaproject/controller/cyr/EmployeeController.java
@@ -0,0 +1,27 @@
+package kr.co.ahaproject.controller.cyr;
+
+import kr.co.ahaproject.dto.MisuDTO;
+import kr.co.ahaproject.entity.Employee;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface EmployeeController {
+    //리스트 페이지로 이동
+    String listAll(Model model);
+
+    //선택보기 페이지 이동
+    String selectOne(Model model);
+
+    String goInsert(Model model);
+
+    String emEditForm(Employee employee, Model model);
+
+    ResponseEntity register(Employee employee);
+
+
+    ResponseEntity update(Employee employee);
+
+}
