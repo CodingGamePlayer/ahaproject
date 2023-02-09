dd8dcae0d37efeaf5204505d1939c8b01950de0d
moosoony <106907863+moosoony@users.noreply.github.com>
2023-02-09 19:17:33 +0900

    Moo (#42)
    * security 인가권한 변경
    
    * 장비등록 코드 가져오기
    
    * 장비정보 입력 코드,장비정보 보기 추가
    
    * 장비정보 입력 코드,장비정보 보기 추가
    
    * 장비정보 입력 초기값 없을 시, 페이지 오류 해결
    
    * 장비정보 보기 수정
    
    * 푸쉬~
    
    * 푸쉬~
    
    * 오늘의푸쉬 오늘너무많음
    
    ---------
    
    Co-authored-by: moomoo <moosoon97@gmail.com>

--- src/main/java/kr/co/ahaproject/apicontroller/moo/MachineApiController.java
+++ src/main/java/kr/co/ahaproject/apicontroller/moo/MachineApiController.java
@@ -2,12 +2,12 @@ package kr.co.ahaproject.apicontroller.moo;
 
 import kr.co.ahaproject.dto.MachineDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 public interface MachineApiController {
 
     ResponseEntity register(MachineDTO machineDTO);
 
     ResponseEntity update(MachineDTO machineDTO);
+
+    ResponseEntity delete(MachineDTO machineDTO);
 }
