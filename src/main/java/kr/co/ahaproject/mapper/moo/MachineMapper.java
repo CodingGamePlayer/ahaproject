6584601c15a6ffefb119b86ca10597e9a3649f05
moosoony <moogyong@naver.com>
2023-02-09 10:51:23 +0900

    푸쉬~

--- src/main/java/kr/co/ahaproject/mapper/moo/MachineMapper.java
+++ src/main/java/kr/co/ahaproject/mapper/moo/MachineMapper.java
@@ -12,7 +12,7 @@ public interface MachineMapper {
     @Insert("INSERT INTO `ahaproject`.`machine` (`m_code`,`m_name`, `m_equip_num`, `m_type`, `m_price`,`m_tax`,`m_date`,`m_file`,`m_etc1`,`m_etc2`,`m_etc3`)" +
     "VALUES (#{machine.m_code}, #{machine.m_name}, #{machine.m_equip_num}, #{machine.m_type}, #{machine.m_price}, #{machine.m_tax}, #{machine.m_date}, #{machine.m_file}, #{machine.m_etc1}, #{machine.m_etc2}, #{machine.m_etc3})")
     int register(@Param("machine") Machine machine);
-
+//
     @Select("SELECT * FROM machine ORDER BY m_id DESC")
     @Results(id = "machineMap", value = {
             @Result(property = "m_id", column = "m_id"),
