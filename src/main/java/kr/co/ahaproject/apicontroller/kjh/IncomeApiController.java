0053fdbacfd152fe58a2e707f1d5d67ce47579c7
지현 <124012755+kohyuny@users.noreply.github.com>
2023-02-10 14:27:00 +0900

    Koko (#46)
    * commit
    
    * commit
    
    * commit
    detail-page.html 추가
    
    * commit
    커밋한다~
    
    * commit
    
    * commit
    
    ---------
    
    Co-authored-by: kohyuny <hahahahaha@hahah.a>
    Co-authored-by: koko <>

--- src/main/java/kr/co/ahaproject/apicontroller/kjh/IncomeApiController.java
+++ src/main/java/kr/co/ahaproject/apicontroller/kjh/IncomeApiController.java
@@ -4,8 +4,12 @@ import kr.co.ahaproject.dto.IncomeOutcomeDTO;
 import kr.co.ahaproject.dto.MisuDTO;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 public interface IncomeApiController {
     ResponseEntity register(IncomeOutcomeDTO ioDTO);
 
     ResponseEntity update(IncomeOutcomeDTO ioDTO);
+    ResponseEntity delete(IncomeOutcomeDTO ioDTO);
+
 }
--- src/main/java/kr/co/ahaproject/controller/kjh/imp/IncomeControllerImp.java
+++ src/main/java/kr/co/ahaproject/controller/kjh/imp/IncomeControllerImp.java
@@ -1,7 +1,12 @@
 package kr.co.ahaproject.controller.kjh.imp;
 
 import kr.co.ahaproject.controller.kjh.IncomeController;
+import kr.co.ahaproject.dto.IncomeOutcomeDTO;
 import kr.co.ahaproject.service.kjh.IncomeService;
+import kr.co.ahaproject.service.kjh.imp.IncomeServiceImp;
+import kr.co.ahaproject.service.kjs.ClientService;
+import kr.co.ahaproject.service.kjs.CompanyService;
+import kr.co.ahaproject.service.mskim.ConstructionService2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,35 +15,60 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user/accounting/income")
+@RequestMapping("/user/accounting")
 public class IncomeControllerImp implements IncomeController {
     @Autowired
-    IncomeService incomeService;
+    IncomeServiceImp incomeService;
+    @Autowired
+    ConstructionService2 constService;
+    @Autowired
+    ClientService clientService;
+    @Autowired
+    CompanyService companyService;
 
     @Override
-    @GetMapping("/list")
+    @GetMapping("/income")
     public String selectAll(Model model) {
         model.addAttribute("ioDTOs", incomeService.selectAll());
         return "user/accounting/income/list";
     }
 
     @Override
-    @GetMapping("/{id}/detail")
-    public String selectOne(@PathVariable int id, Model model) {
-        model.addAttribute("ioDTO", incomeService.selectOne(id));
-        return "user/accounting/income/detail";
+    @GetMapping("/income-form")
+    public String registerForm(Model model) {
+        model.addAttribute("companyDTOs", companyService.selectAll());
+        model.addAttribute("clientDTOs", clientService.selectAll());
+        model.addAttribute("constructionDTOs", constService.selectAll());
+
+        return "user/accounting/income/form";
     }
 
-    @Override
-    @GetMapping("/register")
-    public String registerForm() {
-        return "user/accounting/income/register";
-    }
 
     @Override
-    @GetMapping("/{id}/update")
-    public String editForm(@PathVariable int id, Model model) {
-        model.addAttribute("ioDTO", incomeService.selectOne(id));
-        return "user/accounting/income/update";
+    @GetMapping("/income-edit")
+    public String editForm(long io_id, Model model) {
+        model.addAttribute("ioDTO", incomeService.selectOne( io_id));
+        model.addAttribute("companyDTOs", companyService.selectAll());
+        model.addAttribute("clientDTOs", clientService.selectAll());
+        model.addAttribute("constructionDTOs", constService.selectAll());
+
+        return "user/accounting/income/edit-form";
     }
+
+    @GetMapping("/test")
+    public String test1(IncomeOutcomeDTO ioDTO, Model model) {
+        IncomeOutcomeDTO ioDTO1 = incomeService.selectRecent(ioDTO);
+        System.out.println(ioDTO1.getIo_id());
+        if(ioDTO1.getIo_id()==null) {
+            ioDTO.setIn_total_value(ioDTO.getIn_supp_value());
+            ioDTO.setIn_collect_remain(ioDTO.getIn_supp_value() - ioDTO.getIn_collect_value());
+        } else {
+            ioDTO.setIn_total_value(ioDTO1.getIn_total_value()+ioDTO.getIn_supp_value());
+            ioDTO.setIn_collect_remain(ioDTO1.getIn_collect_remain()+ioDTO.getIn_supp_value()-ioDTO.getIn_collect_value());
+        }
+        return "user/accounting/income/test";
+    }
+
+
 }
+
