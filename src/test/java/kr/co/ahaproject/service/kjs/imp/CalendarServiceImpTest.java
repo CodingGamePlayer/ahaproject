65a849e65f50ba538ee36f8d06650b2e3904eaef
김종식 <otw1917@naver.com>
2023-02-11 22:01:44 +0900

    데이터 알고리즘 정리

--- src/test/java/kr/co/ahaproject/service/kjs/imp/CalendarServiceImpTest.java
+++ src/test/java/kr/co/ahaproject/service/kjs/imp/CalendarServiceImpTest.java
@@ -1,15 +1,18 @@
 package kr.co.ahaproject.service.kjs.imp;
 
 import kr.co.ahaproject.dto.CalendarDTO;
+import kr.co.ahaproject.dto.CountMachRentDTO;
 import kr.co.ahaproject.service.kjs.CalendarService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 @Slf4j
@@ -32,11 +35,40 @@ class CalendarServiceImpTest {
 
         Map<String, List> map = new HashMap<>();
 
-        for(int i=1; i <= 12; i++){
+        List<CalendarDTO> calendarDTOS = calendarService.selectAll();
 
-            String month = String.format("%02d", i);
-            log.info(month);
+        List<String> yearArr = new ArrayList<>();
 
+        for (int i = 0; i < calendarDTOS.size(); i++) {
+            String year = calendarDTOS.get(i).getStart().substring(0, 4);
+
+
+            if (!yearArr.contains(year)) {
+                yearArr.add(year);
+            }
         }
+
+//        log.info(yearArr.toString());
+
+        List<CountMachRentDTO> machRentDTOList = yearArr.stream()
+                .map(s -> new CountMachRentDTO(s))
+                .collect(Collectors.toList());
+
+//        log.info(machRentDTOList.toString());
+
+        for (int i = 0; i < calendarDTOS.size(); i++) {
+            String year = calendarDTOS.get(i).getStart().substring(0, 4);
+            String month = calendarDTOS.get(i).getStart().substring(5, 7);
+
+            for(int j = 0; j < machRentDTOList.size(); j++){
+                if(machRentDTOList.get(j).getYear().equals(year)){
+                    machRentDTOList.get(j).increaseCount(month);
+                }
+            }
+        }
+
+        machRentDTOList.forEach(countMachRentDTO -> log.info(String.valueOf(countMachRentDTO)));
+
+
     }
 }
