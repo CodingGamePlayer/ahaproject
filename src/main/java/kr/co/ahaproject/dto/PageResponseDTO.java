67f7f20a34d8561028657101a75573abb7b9c371
김종식 <otw1917@naver.com>
2023-02-13 14:20:36 +0900

    페이징 기능 구현

---
+++ src/main/java/kr/co/ahaproject/dto/PageResponseDTO.java
@@ -0,0 +1,46 @@
+package kr.co.ahaproject.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PageResponseDTO<E> {
+
+    private int page;
+
+    private int size;
+
+    private int total;
+
+    private int start;
+
+    private int end;
+
+    private boolean prev;
+
+    private boolean next;
+
+    private List<E> dtoList;
+
+    @Builder(builderMethodName = "withAll")
+    public PageResponseDTO(PageRequestDTO pageRequestDTO, List<E> dtoList, int total){
+        this.page = pageRequestDTO.getPage();
+        this.size = pageRequestDTO.getSize();
+
+        this.total = total;
+        this.dtoList = dtoList;
+
+        this.end = (int) ((Math.ceil(this.page/10)) * 10);
+        this.start = end-9;
+
+        int last = (int) Math.ceil(total/ (double)size);
+
+        this.end = end > last ? last : end;
+
+        this.prev = this.start > 1;
+
+        this.next = total > this.end * this.size;
+    }
+}
