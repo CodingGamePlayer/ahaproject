ca13961ca6973f154cdcb2b6ce4eabf8765dfe9d
moosoony <106907863+moosoony@users.noreply.github.com>
2023-02-13 16:45:08 +0900

    Moo (#65)
    * security 인가권한 변경
    
    * 장비등록 코드 가져오기
    
    * 장비정보 입력 코드,장비정보 보기 추가
    
    * 장비정보 입력 코드,장비정보 보기 추가
    
    * 장비정보 입력 초기값 없을 시, 페이지 오류 해결
    
    * 장비정보 보기 수정
    
    * 푸쉬~
    
    * 푸쉬~
    
    * 오늘의푸쉬 오늘너무많음
    
    * 장비임대 조회, 등록, 수정, 삭제 완료
    
    * 인건비 수정 중
    
    * 인건비 수정 중
    
    * 장비임대 rent_bill_date 추가
    인건비 오류 처리 완료
    
    * 장비임대 dto 오류 수정
    장비임대 구분 추가
    
    * 인건비 완료
    
    ---------
    
    Co-authored-by: moomoo <moosoon97@gmail.com>

--- src/main/java/kr/co/ahaproject/mapper/moo/LaborCostMapper.java
+++ src/main/java/kr/co/ahaproject/mapper/moo/LaborCostMapper.java
@@ -1,11 +1,47 @@
 package kr.co.ahaproject.mapper.moo;
 
+import kr.co.ahaproject.dto.LaborCostListDTO;
 import kr.co.ahaproject.entity.LaborCost;
+import kr.co.ahaproject.entity.MachRent;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface LaborCostMapper {
+    @Insert("INSERT INTO labor_cost(emp_code, cst_code, cl_code, lc_value, lc_date, lc_regit)" +
+            "VALUES (#{laborCost.emp_code}, #{laborCost.cst_code}, #{laborCost.cl_code}, #{laborCost.lc_value}, #{laborCost.lc_date}, #{laborCost.lc_regit})")
+        int register(@Param("laborCost") LaborCost laborCost);
 
+    @Select("SELECT * FROM labor_cost ORDER BY lc_id DESC")
+    @Results(id = "laborCostMap", value = {
+            @Result(property = "lc_id", column = "lc_id"),
+            @Result(property = "emp_code", column = "emp_code"),
+            @Result(property = "cst_code", column = "cst_code"),
+            @Result(property = "cl_code", column = "cl_code"),
+            @Result(property = "lc_value", column = "lc_value"),
+            @Result(property = "lc_date", column = "lc_date"),
+            @Result(property = "lc_regit", column = "lc_regit")
+    })
+    List<LaborCost> selectAll();
+
+    @Select("SELECT * FROM labor_cost WHERE lc_id = #{laborCost.lc_id}")
+    @ResultMap("laborCostMap")
+    LaborCost findById(@Param("laborCost") LaborCost laborCost);
+
+    @Update("UPDATE labor_cost SET " +
+            "emp_code = #{laborCost.emp_code}, cst_code = #{laborCost.cst_code}, " +
+            "cl_code = #{laborCost.cl_code}, lc_value = #{laborCost.lc_value}, " +
+            "lc_date = #{laborCost.lc_date}, lc_regit = #{laborCost.lc_regit} " +
+            "WHERE lc_id = #{laborCost.lc_id}")
+    int update(@Param("laborCost") LaborCost laborCost);
+
+    @Delete("DELETE FROM labor_cost WHERE lc_id = #{laborCost.lc_id}")
+    int delete(@Param("laborCost")LaborCost laborCost);
+
+    @Select("SELECT A.lc_id, B.emp_name, C.cst_name, D.cp_name, A.lc_value, A.lc_date, A.lc_regit " +
+            "FROM labor_cost A JOIN employee B ON A.emp_code = B.emp_code " +
+            "JOIN construction C ON A.cst_code = C.cst_code " +
+            "JOIN client D on A.cl_code = D.cl_code ORDER BY A.lc_id DESC")
+    List<LaborCostListDTO> selectAllName();
 }
