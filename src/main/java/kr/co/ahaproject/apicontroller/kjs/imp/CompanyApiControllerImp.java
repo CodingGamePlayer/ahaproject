365aaa414dfef3224ae28f2768e2126987a2360c
SoochanP <121854793+SoochanP@users.noreply.github.com>
2023-02-13 10:35:56 +0900

    날짜변경메소드 추가!

--- src/main/java/kr/co/ahaproject/apicontroller/kjs/imp/CompanyApiControllerImp.java
+++ src/main/java/kr/co/ahaproject/apicontroller/kjs/imp/CompanyApiControllerImp.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
 import kr.co.ahaproject.apicontroller.kjs.CompanyApiController;
 import kr.co.ahaproject.dto.CompanyDTO;
+import kr.co.ahaproject.service.AhaCommonMethod;
 import kr.co.ahaproject.service.kjs.CompanyService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -32,6 +33,14 @@ public class CompanyApiControllerImp implements CompanyApiController{
 	@PostMapping(value = "/company", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<CompanyDTO> register(@RequestBody CompanyDTO companyDTO) {
 		// TODO Auto-generated method stub
+
+//		날짜변경 메소드
+		String after = new AhaCommonMethod().changeDate(companyDTO.getCp_establish());
+		companyDTO.setCp_establish(after);
+		
+		String after1 = new AhaCommonMethod().changeDate(companyDTO.getCp_open());
+		companyDTO.setCp_establish(after1);
+		
 		if(companyDTO == null) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
@@ -45,11 +54,21 @@ public class CompanyApiControllerImp implements CompanyApiController{
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
 
+	
+//	회사정보 수정
 	@Override
 	@Operation(summary = "update for company,put", description = "회사정보 수정")
 	@PutMapping(value = "/company", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<CompanyDTO> update(@RequestBody CompanyDTO companyDTO) {
 		// TODO Auto-generated method stub
+
+//		날짜변경 메소드
+		String after = new AhaCommonMethod().changeDate(companyDTO.getCp_establish());
+		companyDTO.setCp_establish(after);
+		
+		String after1 = new AhaCommonMethod().changeDate(companyDTO.getCp_open());
+		companyDTO.setCp_establish(after1);
+
 		if(companyDTO == null) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
